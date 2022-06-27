6973fa5f2c56f871381c02071362612de1539667
Andrea Lin <andre93.lin@gmail.com>
2022-06-27 20:21:51 -0300

    set up with mui

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+// A custom theme for this app
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3FC1C9',
+    },
+    secondary: {
+      main: '#FC5185',
+    },
+  },
+});
+
+export default theme;
